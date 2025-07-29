ffbefd6fc47935a34d7efd128cf5026e65266249
Steven Rieder <s.rieder@uva.nl>
2025-07-29 15:36:08 +0200

    register other formats

--- src/lo2t/decode/__init__.py
+++ src/lo2t/decode/__init__.py
@@ -2,14 +2,18 @@
 Functions for decoding GCN notices
 """
 
-from .base import registered_gcn_processors
-from .base import read_gcn_notice
+from .base import process_gcn_notice
 from .ligo import LigoProcessor
 from .icecube import IcecubeProcessor
+from .einsteinprobe import EinsteinprobeProcessor
+from .svom import SvomProcessor
+from .swift import SwiftProcessor
 
 
 LigoProcessor.register()
 IcecubeProcessor.register()
+EinsteinprobeProcessor.register()
+SvomProcessor.register()
+SwiftProcessor.register()
 
-
-__all__ = ["read_gcn_notice"]
+__all__ = ["process_gcn_notice"]
