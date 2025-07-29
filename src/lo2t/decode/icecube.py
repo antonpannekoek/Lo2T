e3229fd00c01b97260a69cb32ee9f4775efad675
Steven Rieder <s.rieder@uva.nl>
2025-07-29 10:06:03 +0200

    register format to parse

--- src/lo2t/decode/icecube.py
+++ src/lo2t/decode/icecube.py
@@ -9,5 +9,7 @@ class IcecubeProcessor(JsonProcessor):
     """
     Processor for IceCube JSON events
     """
+
+    provided_formats = ["icecube"]
     def __init__(self, message, verbose=False):
         super().__init__(message, verbose=verbose)
