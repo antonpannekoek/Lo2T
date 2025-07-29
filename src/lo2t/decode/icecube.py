f49e63b28b41dafb5bf9feac0f2971b797375449
Steven Rieder <s.rieder@uva.nl>
2025-07-29 15:36:31 +0200

    style update

--- src/lo2t/decode/icecube.py
+++ src/lo2t/decode/icecube.py
@@ -1,5 +1,5 @@
 """
-Process IceCube JSON events
+Processor for IceCube JSON events
 """
 
 from .base import JsonProcessor
@@ -9,7 +9,7 @@ class IcecubeProcessor(JsonProcessor):
     """
     Processor for IceCube JSON events
     """
-
     provided_formats = ["icecube"]
+
     def __init__(self, message, verbose=False):
         super().__init__(message, verbose=verbose)
