76d42eb2f5b7b04e692e18ba3a6b7f0847d3a477
Steven Rieder <s.rieder@uva.nl>
2025-08-13 13:48:34 +0200

    initial SWIFT processor

--- src/lo2t/decode/swift.py
+++ src/lo2t/decode/swift.py
@@ -1,11 +1,14 @@
 """
-Processor for Swift messages
+Processor for Swift messages.
+
+
+
 """
+import sys
+import argparse
+from .base import VoeventProcessor
 
-from .base import JsonProcessor
-
-
-class SwiftProcessor(JsonProcessor):
+class SwiftProcessor(VoeventProcessor):
     """
     Processor for Swift messages
     """
@@ -13,3 +16,20 @@ class SwiftProcessor(JsonProcessor):
 
     def __init__(self, message, verbose=False):
         super().__init__(message, verbose=verbose)
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("-v", "--verbose", action="store_true")
+    parser.add_argument("-r", "--record", nargs="+", help="Swift records to parse")
+    args = parser.parse_args()
+    for messagefile in args.record:
+        with open(messagefile, "rb") as f:
+            processor = SwiftProcessor(f, verbose=args.verbose)
+            processor.process()
+            print(processor.get_position())
+            print(processor.get_observation_time())
+
+
+if __name__ == "__main__":
+    main()
